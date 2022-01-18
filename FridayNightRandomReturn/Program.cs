931937564a41d0b1ff834e91228d44c85bcf80f3
fgrunber <96977461+fgrunber@users.noreply.github.com>
2022-01-18 16:31:10 +0200

    first commit

---
+++ FridayNightRandomReturn/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FridayNightRandomReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomFood, randomDrinks, randomMovies;
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salad" };
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite" };
+            string[] movies = { "The Family", "Shot Caller", "The Nice Guys", "FF1" };
+
+            randomFood = PickRandomElementFromArray(foods);
+            randomDrinks = PickRandomElementFromArray(drinks);
+            randomMovies = PickRandomElementFromArray(movies);
+
+            Console.WriteLine($"Tonight you will have some {randomFood} with {randomDrinks} and watch {randomMovies}");
+
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+
+        private static string PickRandomElementFromArray(string[] someArray)
+        {
+            string randomElement = someArray[GenerateRandomIndex(someArray)];
+            return randomElement;
+        }
+    }
+}
